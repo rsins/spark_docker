310a180bff757a873805d4ea14718ab8f87ce652
rsins <ravikantsingh.here@gmail.com>
2021-01-31 10:28:52 +0530

    initial copy

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# generate ssh key
+echo "Y" | ssh-keygen -t rsa -P "" -f configs/id_rsa
+
+# Hadoop build
+docker build -f ./hadoop/Dockerfile . -t sciencepal/hadoop_cluster:hadoop
+
+# Spark
+docker build -f ./spark/Dockerfile . -t sciencepal/hadoop_cluster:spark
+
+# PostgreSQL Hive Metastore Server
+docker build -f ./postgresql-hms/Dockerfile . -t sciencepal/hadoop_cluster:postgresql-hms
+
+# Hive
+docker build -f ./hive/Dockerfile . -t sciencepal/hadoop_cluster:hive
+
+# Nifi
+docker build -f ./nifi/Dockerfile . -t sciencepal/hadoop_cluster:nifi
+
+# Edge
+docker build -f ./edge/Dockerfile . -t sciencepal/hadoop_cluster:edge
+
+# hue
+docker build -f ./hue/Dockerfile . -t sciencepal/hadoop_cluster:hue
+
+# zeppelin
+docker build -f ./zeppelin/Dockerfile . -t sciencepal/hadoop_cluster:zeppelin
