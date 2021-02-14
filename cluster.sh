33e5ebe25930fcd4bbb430872061cec3b65a118d
rsins <ravikantsingh.here@gmail.com>
2021-02-14 12:05:26 +0530

    modified cluster script

--- cluster.sh
+++ cluster.sh
@@ -5,47 +5,47 @@ function startServices {
   docker start nodemaster node2 node3
   sleep 5
 
-  echo ">> Starting hdfs ..."
+  echo " >> Starting hdfs ..."
   docker exec -u hadoop -it nodemaster start-dfs.sh
   sleep 5
   
-  echo ">> Starting yarn ..."
+  echo " >> Starting yarn ..."
   docker exec -u hadoop -d nodemaster start-yarn.sh
   sleep 5
   
-  echo ">> Starting MR-JobHistory Server ..."
+  echo " >> Starting MR-JobHistory Server ..."
   docker exec -u hadoop -d nodemaster mr-jobhistory-daemon.sh start historyserver
   sleep 5
   
-  echo ">> Starting Spark ..."
+  echo " >> Starting Spark ..."
   docker exec -u hadoop -d nodemaster start-master.sh
   docker exec -u hadoop -d node2 start-slave.sh nodemaster:7077
   docker exec -u hadoop -d node3 start-slave.sh nodemaster:7077
   sleep 5
   
-  echo ">> Starting Spark History Server ..."
+  echo " >> Starting Spark History Server ..."
   docker exec -u hadoop nodemaster start-history-server.sh
   sleep 5
   
-  echo ">> Preparing hdfs for hive ..."
+  echo " >> Preparing hdfs for hive ..."
   docker exec -u hadoop -it nodemaster hdfs dfs -mkdir -p /tmp
   docker exec -u hadoop -it nodemaster hdfs dfs -mkdir -p /user/hive/warehouse
   docker exec -u hadoop -it nodemaster hdfs dfs -chmod g+w /tmp
   docker exec -u hadoop -it nodemaster hdfs dfs -chmod g+w /user/hive/warehouse
   sleep 5
   
-  echo ">> Starting Hive Metastore ..."
+  echo " >> Starting Hive Metastore ..."
   docker exec -u hadoop -d nodemaster hive --service metastore
   docker exec -u hadoop -d nodemaster hive --service hiveserver2
   
-  echo ">> Starting Nifi Server ..."
+  echo " >> Starting Nifi Server ..."
   docker exec -u hadoop -d nifi /home/hadoop/nifi/bin/nifi.sh start
   
-  echo ">> Starting kafka & Zookeeper ..."
+  echo " >> Starting kafka & Zookeeper ..."
   docker exec -u hadoop -d edge /home/hadoop/kafka/bin/zookeeper-server-start.sh -daemon  /home/hadoop/kafka/config/zookeeper.properties
   docker exec -u hadoop -d edge /home/hadoop/kafka/bin/kafka-server-start.sh -daemon  /home/hadoop/kafka/config/server.properties
   
-  echo ">> Starting Zeppelin ..."
+  echo " >> Starting Zeppelin ..."
   docker exec -u hadoop -d zeppelin /home/hadoop/zeppelin/bin/zeppelin-daemon.sh start
 
   echo 
@@ -65,14 +65,14 @@ function startServices {
 }
 
 function stopServices {
-  echo ">> Stopping Spark Master and slaves ..."
+  echo " >> Stopping Spark Master and slaves ..."
   docker exec -u hadoop -d nodemaster stop-master.sh
   docker exec -u hadoop -d node2 stop-slave.sh
   docker exec -u hadoop -d node3 stop-slave.sh
   docker exec -u hadoop -d nifi /home/hadoop/nifi/bin/nifi.sh stop
   docker exec -u hadoop -d zeppelin /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
 
-  echo ">> Stopping containers ..."
+  echo " >> Stopping containers ..."
   docker stop nodemaster node2 node3 edge hue nifi zeppelin psqlhms
 }
 
@@ -80,22 +80,88 @@ if [[ $1 = "install" ]]; then
   docker network create --subnet=172.20.0.0/16 sparknet # create custom network
 
   # Starting Postresql Hive metastore
-  echo ">> Starting postgresql hive metastore ..."
-  docker run -d --net sparknet --ip 172.20.1.4 --hostname psqlhms --name psqlhms -e POSTGRES_PASSWORD=hive -it rsins/spark_cluster:postgresql-hms
+  echo " >> Starting postgresql hive metastore ..."
+  docker run -d --net sparknet                          \
+  	            --hostname psqlhms                      \
+  	            --name psqlhms                          \
+  	            --ip 172.20.1.4                         \
+  	            -e POSTGRES_PASSWORD=hive               \
+  	            -it rsins/spark_cluster:postgresql-hms
   sleep 5
   
   # 3 nodes
-  echo ">> Starting master and worker nodes ..."
-  docker run -d --net sparknet --ip 172.20.1.1 -p 8088:8088 -p 18080:18080 -p 4040:4040 -p 6066:6066 -p 7077:7077 -p 9000:9000 --hostname nodemaster --add-host node2:172.20.1.2 --add-host node3:172.20.1.3 --name nodemaster -it rsins/spark_cluster:hive
-  docker run -d --net sparknet --ip 172.20.1.2 --hostname node2 --add-host nodemaster:172.20.1.1 --add-host node3:172.20.1.3 --name node2 -it rsins/spark_cluster:spark
-  docker run -d --net sparknet --ip 172.20.1.3 --hostname node3 --add-host nodemaster:172.20.1.1 --add-host node2:172.20.1.2 --name node3 -it rsins/spark_cluster:spark
-  docker run -d --net sparknet --ip 172.20.1.5 --hostname edge --add-host nodemaster:172.20.1.1 --add-host node2:172.20.1.2 --add-host node3:172.20.1.3 --add-host psqlhms:172.20.1.4 --name edge -it rsins/spark_cluster:edge 
-  docker run -d --net sparknet --ip 172.20.1.6 -p 8080:8080 --hostname nifi --add-host nodemaster:172.20.1.1 --add-host node2:172.20.1.2 --add-host node3:172.20.1.3 --add-host psqlhms:172.20.1.4 --name nifi -it rsins/spark_cluster:nifi 
-  docker run -d --net sparknet --ip 172.20.1.7 -p 8888:8888 --hostname huenode --add-host edge:172.20.1.5 --add-host nodemaster:172.20.1.1 --add-host node2:172.20.1.2 --add-host node3:172.20.1.3 --add-host psqlhms:172.20.1.4 --name hue -it rsins/spark_cluster:hue
-  docker run -d --net sparknet --ip 172.20.1.8 -p 8081:8081 --hostname zeppelin --add-host edge:172.20.1.5 --add-host nodemaster:172.20.1.1 --add-host node2:172.20.1.2 --add-host node3:172.20.1.3 --add-host psqlhms:172.20.1.4 --name zeppelin -it rsins/spark_cluster:zeppelin
+  echo " >> Starting master and worker nodes ..."
+  docker run -d --net sparknet                          \
+  	            --hostname nodemaster                   \
+  	            --name nodemaster                       \
+  	            --ip 172.20.1.1                         \
+  	            -p 8088:8088                            \
+  	            -p 18080:18080                          \
+  	            -p 4040:4040                            \
+  	            -p 6066:6066                            \
+  	            -p 7077:7077                            \
+  	            -p 9000:9000                            \
+  	            --add-host node2:172.20.1.2             \
+  	            --add-host node3:172.20.1.3             \
+  	            -it rsins/spark_cluster:hive
+  docker run -d --net sparknet                          \
+  	            --hostname node2                        \
+  	            --name node2                            \
+  	            --ip 172.20.1.2                         \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node3:172.20.1.3             \
+  	            -it rsins/spark_cluster:spark
+  docker run -d --net sparknet                          \
+  	            --hostname node3                        \
+  	            --name node3                            \
+  	            --ip 172.20.1.3                         \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node2:172.20.1.2             \
+  	            -it rsins/spark_cluster:spark
+  docker run -d --net sparknet                          \
+  	            --hostname edge                         \
+  	            --name edge                             \
+  	            --ip 172.20.1.5                         \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node2:172.20.1.2             \
+  	            --add-host node3:172.20.1.3             \
+  	            --add-host psqlhms:172.20.1.4           \
+  	            -it rsins/spark_cluster:edge 
+  docker run -d --net sparknet                          \
+  	            --hostname nifi                         \
+  	            --name nifi                             \
+  	            --ip 172.20.1.6                         \
+  	            -p 8080:8080                            \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node2:172.20.1.2             \
+  	            --add-host node3:172.20.1.3             \
+  	            --add-host psqlhms:172.20.1.4           \
+  	            -it rsins/spark_cluster:nifi 
+  docker run -d --net sparknet                          \
+  	            --hostname huenode                      \
+  	            --name hue                              \
+  	            --ip 172.20.1.7                         \
+  	            -p 8888:8888                            \
+  	            --add-host edge:172.20.1.5              \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node2:172.20.1.2             \
+  	            --add-host node3:172.20.1.3             \
+  	            --add-host psqlhms:172.20.1.4           \
+  	            -it rsins/spark_cluster:hue
+  docker run -d --net sparknet                          \
+  	            --hostname zeppelin                     \
+  	            --name zeppelin                         \
+  	            --ip 172.20.1.8                         \
+  	            -p 8081:8081                            \
+  	            --add-host edge:172.20.1.5              \
+  	            --add-host nodemaster:172.20.1.1        \
+  	            --add-host node2:172.20.1.2             \
+  	            --add-host node3:172.20.1.3             \
+  	            --add-host psqlhms:172.20.1.4           \
+  	            -it rsins/spark_cluster:zeppelin
 
   # Format nodemaster
-  echo ">> Formatting hdfs ..."
+  echo " >> Formatting hdfs ..."
   docker exec -u hadoop -it nodemaster hdfs namenode -format
   startServices
   exit
@@ -112,7 +178,7 @@ if [[ $1 = "uninstall" ]]; then
   stopServices
   docker rmi rsins/spark_cluster:hadoop rsins/spark_cluster:spark rsins/spark_cluster:hive rsins/spark_cluster:postgresql-hms rsins/spark_cluster:hue rsins/spark_cluster:edge rsins/spark_cluster:nifi rsins/spark_cluster:zeppelin -f
        # can comment it later after testing, this will clear containers but leave images
-  # docker ps -a | awk -F' ' '{print $1}' | tail -n +2 | xargs -I CC docker rm CC
+  #docker ps -a | awk -F' ' '{print $1}' | tail -n +2 | xargs -I CC docker rm CC
   docker network rm sparknet
   docker system prune -f
   exit
@@ -129,9 +195,10 @@ if [[ $1 = "pull_images" ]]; then
   exit
 fi
 
-echo "Usage: $0 [ pull_images | install | start | stop | uninstall ]"
-echo "       pull_images  - download all docker images"
-echo "       install      - Prepare to run and start for first time all containers"
-echo "       start        - start existing containers"
-echo "       stop         - stop running processes"
-echo "       uninstall    - remove all docker images"
+echo " Usage: $0 [ pull_images | install | start | stop | uninstall ]"
+echo "        pull_images  - download all docker images"
+echo "        install      - Prepare to run and start for first time all containers"
+echo "        start        - start existing containers"
+echo "        stop         - stop running processes"
+echo "        uninstall    - remove all docker images"
+
